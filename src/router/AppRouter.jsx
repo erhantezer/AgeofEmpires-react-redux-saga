f290d2bbfeb8e5e2939b5a769d7e83282ac56c46
Erhan <erhantezer68@gmail.com>
2023-01-29 15:10:12 +0300

    updated

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Details from '../components/Details'
+import Home from '../components/Home'
+import Units from '../components/Units'
 
 const AppRouter = () => {
-  return (
-    <div>AppRouter</div>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='units' element={<Units />} />
+                <Route path='details/:unitId' element={<Details />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default AppRouter
